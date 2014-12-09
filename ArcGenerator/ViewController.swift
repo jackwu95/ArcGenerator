7e6242e20235c40306f4ceb485de815ebb09aa1d
jackwu <jackwu95@gmail.com>
2014-12-09 02:32:35 -0500

    Compensate for line width in radius and adjust for screen scale properly. Change indent to 2 spaces.

--- ArcGenerator/ViewController.swift
+++ ArcGenerator/ViewController.swift
@@ -13,7 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let params = ArcParams()
+        let params = ArcParams(radius: 100,
+            lineWidth: 10,
+            padding: 0,
+            startAngle: 3.0*M_PI/2.0,
+            clockwise: true,
+            color:  UIColor.whiteColor(),
+            name: "arc",
+            frameCount: 360)
         let generator = ArcGenerator()
         generator.generateArcs(params)
     }
